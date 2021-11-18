495fa11bfca49d118e14eeeb58730022f97f08ab
Gerasim Gerasimov <dialix@yandex.ru>
2021-11-18 18:43:36 +0700

    продолжаю парсить хекс

--- Info/HexReader/src/index.ts
+++ Info/HexReader/src/index.ts
@@ -5,13 +5,47 @@ import fs = require ("fs");
 
 console.log('Start Hex Reader');
 const fileContent: Array<string> = fs.readFileSync('./src/stm32F4.hex').toString().split("\n");
-console.log(fileContent);
-console.log('Stop Hex Reader');
 
 function getMemoryAreas(content:Array<string>) {
-  content.forEach(element => {
-    console.debug(element);
-  });
+  var segmentAddr: number = 0;
+  for (const idx in content) {
+    const hexstr: string = content[idx];
+    if (getHexSrtLenght(hexstr)) {
+      const {isAddition, Addr, hexAddr} = isAdditionSegmentAddress(hexstr);
+      if (isAddition) {
+        segmentAddr = Addr;
+        console.log(hexAddr);
+      } else {
+
+      }
+    } else {
+      if (isEndOfHex(hexstr)) {
+        console.log('End Of Hex', hexstr);
+        break;
+      }
+    }
+  };
+}
+
+function isAdditionSegmentAddress(str: string): {isAddition: boolean, Addr: number, hexAddr:string}{
+  //:020000040800F2
+  const code: string = str.slice(7,9);
+  return (code == '04')
+         ? {isAddition: true, Addr:parseInt(`0x${str.slice(9,13)}0000`), hexAddr:`0x${str.slice(9,13)}0000`}
+         : {isAddition: false, Addr:0, hexAddr:'0x00000000'};
+}
+
+function getHexSrtLenght(str: string): number {
+  const hexStr: string = `0x${str.slice(1,3)}`;
+  const len: number = parseInt(hexStr);
+  return len;
+}
+
+function isEndOfHex(str: string): boolean {
+  const code: string = str.slice(7,9);
+  return (code == '01');
 }
 
 getMemoryAreas(fileContent);
+
+console.log('Hex Reader has Done');
