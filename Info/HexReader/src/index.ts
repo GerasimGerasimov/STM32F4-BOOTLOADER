942dd8514d6ba07297fe9a12cbcceae6f1af5e13
Gerasim Gerasimov <dialix@yandex.ru>
2021-11-23 15:20:38 +0700

    1) выдаю массив стартовых адресов стираемых страниц, без дубликатов. 2) добавил адреса страниц LPC2378 3) проверил работу выдачи стираемых страниц на STM32F407, STM32F405, LPC2378

--- Info/HexReader/src/index.ts
+++ Info/HexReader/src/index.ts
@@ -1,7 +1,7 @@
 import fs = require ("fs");
 import { getUsageMemoryAddresAndSize} from "./hex";
 import { TFlashSegmen } from "./hextypes";
-import { getErasedPages, getFlashPages } from "./mcu";
+import { getErasedPages} from "./mcu";
 //import path = require('path');
 //const dir: string = path.resolve(__dirname,'');
 
@@ -11,18 +11,7 @@ const fileContent: Array<string> = fs.readFileSync('./src/stm32F405.hex').toStri
 const Areas: Array<TFlashSegmen> = getUsageMemoryAddresAndSize(fileContent);
 
 Areas.forEach((item)=>{console.log(JSON.stringify(item))});
-//console.log(JSON.stringify(Areas, null, '\t'));
-getFlashPages().forEach((item)=>{
-  console.log(JSON.stringify(item));
-});
 
-console.log(getErasedPages(
-  [
-    {start:"0x08000000", size: 61368},
-    {start:"0x080a0000", size: 104},
-    {start:"0x080c0000", size: 104},
-    {start:"0x0800DD81", size: "main"},
-    {start:"0x080e0000", size: 4}
-  ]));
+console.log(getErasedPages(Areas));
 
 console.log('Hex Reader has Done');
--- Info/HexReader/src/mcu.ts
+++ Info/HexReader/src/mcu.ts
@@ -20,7 +20,7 @@ result:
 
 */
 export function getErasedPages( domain: Array<TFlashSegmen>): Array<string> {
-  const res:Array<string> = [];
+  const res:Set<string> = new Set();
   const pages: Array<TFlashSegmen> = getFlashPages();
   domain.forEach((value)=>{
     if (value.size !== 'main') {
@@ -29,13 +29,13 @@ export function getErasedPages( domain: Array<TFlashSegmen>): Array<string> {
         const page:TFlashSegmen = pages[idx];
         const page_set = {start: parseInt(page.start), end: parseInt(page.start) + Number(page.size)};
         if (intersection(domain_set, page_set)) {
-          res.push(page.start);
+          res.add(page.start);
           console.log(page);
         }
       }
     }
   })
-  return res;
+  return [...res];
 }
 
 function intersection(A: {start:number, end: number}, B:{start: number, end: number}): boolean {
@@ -48,21 +48,54 @@ function intersection(A: {start:number, end: number}, B:{start: number, end: num
 }
 
 export function getFlashPages(): Array<TFlashSegmen> {
-  const FlashPagesSTM32F407IGT: Array<TFlashSegmen> = [
-    {start: '0x08000000', size: 0x03FFF},//16K
-    {start: '0x08004000', size: 0x03FFF},//16K
-    {start: '0x08008000', size: 0x03FFF},//16K
-    {start: '0x0800C000', size: 0x03FFF},//16K
 
-    {start: '0x08010000', size: 0x0FFFF},//64K
-
-    {start: '0x08020000', size: 0x1FFFF},//128K
-    {start: '0x08040000', size: 0x1FFFF},//128K
-    {start: '0x08060000', size: 0x1FFFF},//128K
-    {start: '0x08080000', size: 0x1FFFF},//128K
-    {start: '0x080A0000', size: 0x1FFFF},//128K
-    {start: '0x080C0000', size: 0x1FFFF},//128K
-    {start: '0x080E0000', size: 0x1FFFF} //128K
-  ];
   return FlashPagesSTM32F407IGT;
 }
+
+const FlashPagesSTM32F407IGT: Array<TFlashSegmen> = [
+  {start: '0x08000000', size: 0x03FFF},//16K
+  {start: '0x08004000', size: 0x03FFF},//16K
+  {start: '0x08008000', size: 0x03FFF},//16K
+  {start: '0x0800C000', size: 0x03FFF},//16K
+
+  {start: '0x08010000', size: 0x0FFFF},//64K
+
+  {start: '0x08020000', size: 0x1FFFF},//128K
+  {start: '0x08040000', size: 0x1FFFF},//128K
+  {start: '0x08060000', size: 0x1FFFF},//128K
+  {start: '0x08080000', size: 0x1FFFF},//128K
+  {start: '0x080A0000', size: 0x1FFFF},//128K
+  {start: '0x080C0000', size: 0x1FFFF},//128K
+  {start: '0x080E0000', size: 0x1FFFF} //128K
+];
+
+const FlashPagesLPC2378: Array<TFlashSegmen> = [
+  {start: '0x00000000', size: 0x0FFF},
+  {start: '0x00001000', size: 0x0FFF},
+  {start: '0x00002000', size: 0x0FFF},
+  {start: '0x00003000', size: 0x0FFF},
+  {start: '0x00004000', size: 0x0FFF},
+  {start: '0x00005000', size: 0x0FFF},
+  {start: '0x00006000', size: 0x0FFF},
+  {start: '0x00007000', size: 0x0FFF},
+  {start: '0x00008000', size: 0x7FFF},
+  {start: '0x00010000', size: 0x7FFF},
+  {start: '0x00018000', size: 0x7FFF},
+  {start: '0x00020000', size: 0x7FFF},
+  {start: '0x00028000', size: 0x7FFF},
+  {start: '0x00030000', size: 0x7FFF},
+  {start: '0x00038000', size: 0x7FFF},
+  {start: '0x00040000', size: 0x7FFF},
+  {start: '0x00048000', size: 0x7FFF},
+  {start: '0x00050000', size: 0x7FFF},
+  {start: '0x00058000', size: 0x7FFF},
+  {start: '0x00060000', size: 0x7FFF},
+  {start: '0x00068000', size: 0x7FFF},
+  {start: '0x00070000', size: 0x7FFF},
+  {start: '0x00078000', size: 0x0FFF},
+  {start: '0x00079000', size: 0x0FFF},
+  {start: '0x0007A000', size: 0x0FFF},
+  {start: '0x0007B000', size: 0x0FFF},
+  {start: '0x0007C000', size: 0x0FFF},
+  {start: '0x0007D000', size: 0x0FFF}
+];
