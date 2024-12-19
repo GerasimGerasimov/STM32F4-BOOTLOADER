f23a89b17ea546d3dbd796a7d35a448ed3427e3d
ThinDrew <andreynikonorov5@gmail.com>
2024-12-19 09:25:16 +0700

    HexReader v1.1.0
    - Добавлено автоматическое определение chunkSize
    - Форматирование

--- Info/HexReader/src/index.ts
+++ Info/HexReader/src/index.ts
@@ -1,9 +1,9 @@
-import fs = require ("fs");
+import fs = require("fs");
 import ComPort from "./comport/comport";
 import { appendCRC16toArray, getCRC16 } from "./crc/crc16";
-import { getUsageMemoryAddresAndSize} from "./hex";
+import { getUsageMemoryAddresAndSize } from "./hex";
 import { TFlashSegmen } from "./hextypes";
-import { ErasedPagesToU8Array, getErasedPages, U16ToU8Array, U32ToU8Array} from "./mcu";
+import { ErasedPagesToU8Array, getErasedPages, U16ToU8Array, U32ToU8Array } from "./mcu";
 import { getResourses } from "./resourses";
 import { getSettingsFormConfigFile } from "./utils/config";
 import { delay } from "./utils/delay";
@@ -13,11 +13,11 @@ const settings: any = getSettingsFormConfigFile();
 
 const fileContent: Array<string> = fs.readFileSync(settings.App).toString().split("\n");
 const Areas: Array<TFlashSegmen> = [...getUsageMemoryAddresAndSize(fileContent, settings.FirmwareCheckInfo),
-                                    ...getResourses(settings.resources || undefined)];
+...getResourses(settings.resources || undefined)];
 
 const COMx: ComPort = new ComPort(settings.COM);
 
-(async () => { 
+(async () => {
   while (true) {
     try {
       await COMx.waitForOpen();
@@ -27,15 +27,16 @@ const COMx: ComPort = new ComPort(settings.COM);
         await startBootloader();
       }
       const AvailiblePages: Array<TFlashSegmen> = await getAvailablePagesList();
-      const ErasedPages:Array<string> = getErasedPages(Areas, AvailiblePages, settings.SkipSectors);
+      const chunkSize = calculateChunkSize(AvailiblePages);
+      const ErasedPages: Array<string> = getErasedPages(Areas, AvailiblePages, settings.SkipSectors);
       await eraseSpecifiedPages(ErasedPages);
-      await downloadCodeToMCU(settings.SkipSectors);
+      await downloadCodeToMCU(settings.SkipSectors, chunkSize);
       await startApplication();
       console.log('Hex Reader has Done');
       process.exit(0);
     } catch (e) {
       await delay(1000);
-      console.log('главЛовушка',e);
+      console.log('главЛовушка', e);
     }
   }
 })();
@@ -44,20 +45,20 @@ async function getID(): Promise<string> {
   const FieldBusAddr: number = 0x01;
   const cmdSource = new Uint8Array([FieldBusAddr, 0x11]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource))
-  const answer: any = await COMx.getCOMAnswer({cmd});
+  const answer: any = await COMx.getCOMAnswer({ cmd });
   const msg: Array<number> = validateAnswer(answer);
-  return Buffer.from(msg.slice(3,-2)).toString('ascii');
+  return Buffer.from(msg.slice(3, -2)).toString('ascii');
 }
 
-async function getAvailablePagesList(): Promise< Array<TFlashSegmen>> {
+async function getAvailablePagesList(): Promise<Array<TFlashSegmen>> {
   const FieldBusAddr: number = 0x01;
   const cmdSource = new Uint8Array([FieldBusAddr, 0xB0, 0x00]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource));
-  const answer: any = await  COMx.getCOMAnswer({cmd});
+  const answer: any = await COMx.getCOMAnswer({ cmd });
   console.log(answer);
   const msg: Array<number> = validateAnswer(answer);
   console.log(Buffer.from(msg).toString('ascii'));
-  const buff: Array<number> = msg.slice(3,-2);
+  const buff: Array<number> = msg.slice(3, -2);
   const str = Buffer.from(buff).toString('ascii');
   console.log(str);
   const res: Array<TFlashSegmen> = JSON.parse(str);
@@ -66,15 +67,17 @@ async function getAvailablePagesList(): Promise< Array<TFlashSegmen>> {
 
 const BOOT_CMD_POSITION = 2;
 
-async function eraseSpecifiedPages(ErasedPages:Array<string>): Promise<any> | never {
+async function eraseSpecifiedPages(ErasedPages: Array<string>): Promise<any> | never {
   console.log(ErasedPages);
   const FieldBusAddr: number = 0x01;
   const ErasedPagesArray: Uint8Array = ErasedPagesToU8Array(ErasedPages);
   const cmdSource = new Uint8Array([FieldBusAddr, 0xB0, 0x01, ...ErasedPagesArray]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource));
-  const answer: any = await COMx.getCOMAnswer({cmd, 
-                                               ChunksEndTime:100,
-                                               timeOut:10000});
+  const answer: any = await COMx.getCOMAnswer({
+    cmd,
+    ChunksEndTime: 100,
+    timeOut: 10000
+  });
   /*TODO timeOut depends of: number of pages, size of page, time to erase*/
   const msg: Array<number> = validateAnswer(answer);
   //if (msg[BOOT_CMD_POSITION] != 1) throw new Error ('Wrong Answer');
@@ -84,14 +87,16 @@ async function eraseSpecifiedPages(ErasedPages:Array<string>): Promise<any> | ne
 async function readMem(U32Addr: number, U16Size: number): Promise<any> {
   const FieldBusAddr: number = 0x01;
   const cmdSource = new Uint8Array([FieldBusAddr, 0xB0, 0x03,
-                                    ...U32ToU8Array(U32Addr),
-                                    ...U16ToU8Array(U16Size)]);
+    ...U32ToU8Array(U32Addr),
+    ...U16ToU8Array(U16Size)]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource))
   /*TODO It is necessary to calculate the chunkend time and timeout
          depending on the required data size and boudrate.*/
-  const answer: any = await COMx.getCOMAnswer({cmd, 
-                                               ChunksEndTime:100,
-                                               timeOut:3800});
+  const answer: any = await COMx.getCOMAnswer({
+    cmd,
+    ChunksEndTime: 100,
+    timeOut: 3800
+  });
   const msg: Array<number> = validateAnswer(answer);
   return msg;
 }
@@ -99,15 +104,17 @@ async function readMem(U32Addr: number, U16Size: number): Promise<any> {
 async function writeMem(U32Addr: number, buff: Uint8Array): Promise<any> {
   const FieldBusAddr: number = 0x01;
   const cmdSource = new Uint8Array([FieldBusAddr, 0xB0, 0x04,
-                                    ...U16ToU8Array(buff.length),
-                                    ...U32ToU8Array(U32Addr),
-                                    ...buff]);
+    ...U16ToU8Array(buff.length),
+    ...U32ToU8Array(U32Addr),
+    ...buff]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource))
   /*TODO It is necessary to calculate the chunkend time and timeout
          depending on the required data size and boudrate.*/
-  const answer: any = await COMx.getCOMAnswer({cmd, 
-                                               ChunksEndTime:1,
-                                               timeOut:3000});
+  const answer: any = await COMx.getCOMAnswer({
+    cmd,
+    ChunksEndTime: 1,
+    timeOut: 3000
+  });
   const msg: Array<number> = validateAnswer(answer);
   return msg;
 }
@@ -116,9 +123,11 @@ async function startApplication(): Promise<any> {
   const FieldBusAddr: number = 0x01;
   const cmdSource = new Uint8Array([FieldBusAddr, 0xB0, 0x02]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource))
-  await COMx.getCOMAnswer({cmd, ChunksEndTime:20,
-                                timeOut:500,
-                                NotRespond: true });
+  await COMx.getCOMAnswer({
+    cmd, ChunksEndTime: 20,
+    timeOut: 500,
+    NotRespond: true
+  });
   return;
 }
 
@@ -129,31 +138,30 @@ function validateAnswer(answer: any): Array<number> | never {
 }
 
 function isItApplication(ID: string): boolean {
-  let sa: Array<string> =  ID.split(' ');
-  let name:string = sa[1] || '';
+  let sa: Array<string> = ID.split(' ');
+  let name: string = sa[1] || '';
   return (name !== 'Bootloader');
 }
 
-async function downloadCodeToMCU(SkipSectors:Array<string>) {
-  const skipArea: Array<number> = SkipSectors.map((value)=>parseInt(value));
+async function downloadCodeToMCU(SkipSectors: Array<string>, chunkSize: number) {
+  const skipArea: Array<number> = SkipSectors.map((value) => parseInt(value));
   try {
-    const chunkSize: number = 1024;
     for (const area of Areas) {
       let StartAddr: number = parseInt(area.start);
       if (skipArea.includes(StartAddr)) continue;
-      const CodeData:Array<number> = area.code;
+      const CodeData: Array<number> = area.code;
       let count: number = Number((area.size !== 'main') ? area.size : 0);
       let idx: number = 0;
       while (count > 0) {
         let trcount: number = (chunkSize > count) ? count : chunkSize;
         count -= trcount;
-        const code: Uint8Array = new Uint8Array(CodeData.slice(idx, idx+trcount));
+        const code: Uint8Array = new Uint8Array(CodeData.slice(idx, idx + trcount));
         await writeMem(StartAddr, code);
-        StartAddr +=trcount;
+        StartAddr += trcount;
         idx += trcount;
       }
     }
-    
+
   } catch (e) {
     console.log('write flash error', e);
   }
@@ -163,9 +171,24 @@ async function startBootloader(): Promise<any> {
   const FieldBusAddr: number = 0x01;
   const cmdSource = new Uint8Array([FieldBusAddr, 0xB0]);
   const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource))
-  await COMx.getCOMAnswer({cmd, ChunksEndTime:20,
-                                timeOut:500,
-                                NotRespond: true });
+  await COMx.getCOMAnswer({
+    cmd, ChunksEndTime: 20,
+    timeOut: 500,
+    NotRespond: true
+  });
   await delay(500);
   return;
+}
+
+function calculateChunkSize(availiblePages: TFlashSegmen[]) {
+  let minSize = 8191;
+
+  for (let i = 0; i < availiblePages.length; i++) {
+    let currentSize = Number(availiblePages[i].size);
+    if (currentSize < minSize) {
+      minSize = currentSize
+    }
+  }
+
+  return minSize + 1;
 }
